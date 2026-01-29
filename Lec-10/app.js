da699c8b4c5288c113c1cfeaf3bddff137f8b010
sachin-tyagi80 <sachintyagi6322@gmail.com>
2026-01-29 20:12:43 +0530

    Passing data to view/Middleware

--- Lec-10/app.js
+++ Lec-10/app.js
@@ -1,14 +1,48 @@
+//Express.js Basics
 const express = require("express");
+let bodyparser = require("body-parser");
 const app = express();
 
-app.get("/product/:id", (req, res) => {
-  console.log(req.params); // { id: '123' }
-  res.send("this ia a product route");
+// app.get("/product/:id", (req, res) => {
+//   console.log(req.params); // { id: '123' }
+//   res.send("this ia a product route");
+// });
+
+// app.get("/todo/:id", (req, res) => {
+//   console.log(req.params.id);
+//   res.send("this is a todo route");
+// });
+
+/////////////////////////////
+//Routing (GET & POST)
+let path = require("path");
+//Template Engine – EJS
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+// middleware
+app.use(bodyparser.urlencoded({ extended: true }));
+
+let obj = {
+  id: 1212,
+};
+//Passing Data to Views (EJS)
+app.get("/", (req, res) => {
+  res.render("index", { obj });
 });
 
-app.get("/todo/:id", (req, res) => {
-  console.log(req.params.id);
-  res.send("this is a todo route");
+let todo = ["coding", "reading books", "go to market"];
+
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+//body-parser (Form Data Handling)
+app.post("/register", (req, res) => {
+  console.log(req.body);
+  res.send("user registered successfully...");
+});
+
+app.get("/todos", (req, res) => {
+  res.render("index1", { todo });
 });
 
 app.listen(3000, () => {
---
+++ Lec-10/hof.js
@@ -0,0 +1,5 @@
+let arr = [1, 2, 3, 4];
+let res = arr.map((el) => {
+  return el + 1;
+});
+console.log(res);
