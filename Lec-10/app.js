a84dc8bd63fa1c3cf4e17b13db727a9b06047bdf
sachin-tyagi80 <sachintyagi6322@gmail.com>
2026-01-30 13:46:48 +0530

    get/post

--- Lec-10/app.js
+++ Lec-10/app.js
@@ -22,28 +22,32 @@ app.set("views", path.join(__dirname, "views"));
 // middleware
 app.use(bodyparser.urlencoded({ extended: true }));
 
-let obj = {
-  id: 1212,
-};
+// let obj = {
+//   id: 1212,
+// };
 //Passing Data to Views (EJS)
-app.get("/", (req, res) => {
-  res.render("index", { obj });
-});
-
-let todo = ["coding", "reading books", "go to market"];
+// app.get("/", (req, res) => {
+//   res.render("index", { obj });
+// });
 
 app.get("/register", (req, res) => {
-  res.render("register");
+  res.render("index");
 });
+
+// let todo = ["coding", "reading books", "go to market"];
+
+// app.get("/register", (req, res) => {
+//   res.render("register");
+// });
 //body-parser (Form Data Handling)
 app.post("/register", (req, res) => {
   console.log(req.body);
   res.send("user registered successfully...");
 });
 
-app.get("/todos", (req, res) => {
-  res.render("index1", { todo });
-});
+// app.get("/todos", (req, res) => {
+//   res.render("index1", { todo });
+// });
 
 app.listen(3000, () => {
   console.log("app is running at port 3000");
