80c0514d9f4b079147fd6979f80e8ed1c760c7a5
sachin-tyagi80 <sachintyagi6322@gmail.com>
2026-04-23 14:02:04 +0530

    JWT

--- Lec-18/app.js
+++ Lec-18/app.js
@@ -1,13 +1,51 @@
-let express=require('express');
-let app=express();
-let jwt=require('jsonwebtoken');
+const express = require('express');
+const app = express();
+const jwt = require('jsonwebtoken');
 require("dotenv").config();
 
-app.get('/register',(req,res)=>{
-  let {username,password}=req.query;
-  jwt.sign({"name":username,"pass":password},process.env.SECRET);
-})
+app.use(express.json());
 
-app.listen(process.env.port,()=>{
-  console.log('app is running at port 3000');
-})
+// Register (token generate)
+app.get('/register', (req, res) => {
+  let { username } = req.query;
+
+  let token = jwt.sign(
+    { name: username },   // ❗ no password
+    process.env.SECRET,
+    { expiresIn: "1h" }
+  );
+
+  res.json({ token });
+});
+
+// Middleware (authenticate)
+const authenticate = (req, res, next) => {
+  let authHeader = req.headers['authorization'];
+
+  if (!authHeader) {
+    return res.status(401).json({ msg: "Token missing" });
+  }
+
+  let token = authHeader.split(" ")[1]; // Bearer token
+
+  jwt.verify(token, process.env.SECRET, (err, user) => {
+    if (err) {
+      return res.status(403).json({ msg: "Invalid token" });
+    }
+
+    req.user = user;
+    next();
+  });
+};
+
+// Protected route
+app.get('/order', authenticate, (req, res) => {
+  res.json({
+    message: "Valid user",
+    user: req.user
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server running on port 3000');
+}); 
